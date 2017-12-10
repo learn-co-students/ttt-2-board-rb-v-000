19ee6f64aec9ac7637cc858015a82aa49f1bd988
Kate Drennen <k8ted11@gmail.com>
2017-12-10 03:33:02 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,15 @@
 # Define the variable board below.
+board = "Tic Tac Toe"
+board = [" ", " ", " ", " "," ", " "," ", " ", " "]
+row1 = [" ", " ", " "]
+row1[0]
+row1[1]
+row1[2]
+row2 = [" ", " ", " "]
+row2[3]
+row2[4]
+row2[5]
+row3 = [" ", " ", " "]
+row3[6]
+row3[7]
+row3[8]
