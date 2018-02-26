59f7dbfd80d8f688eeb2feabb3ca0dc1a62d776b
Abba Raskin <abiraskin@gmail.com>
2018-02-26 03:38:58 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,1 @@
-board = [" Top left", "Top middle", "Top right", "Middle left", "Middle middle", "Middle Right", "Bottom left", "Bottom middle", "Bottom right"]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
