fcdad61fbec9b571fa13faa1e7329d515decbc27
Elizabeth Smith <elizabeth.smith1121@gmail.com>
2019-04-23 14:31:00 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,6 @@
 # Define the variable board below.
+<<<<<<< HEAD
 board = [" ", " ", " ", " ", " "," ", " ", " ", " "]
+=======
+board = ["row 1", "row 2", "row 3", "row 4", "row 5","row 6", "row 7", "row 8", "row 9"]
+>>>>>>> 5596e7c1c8f37267e47286555e7a0e5ecd4110db
