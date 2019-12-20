b54bd264b4b7dd0ffd65a497809dc58059c3d6ca
Chris Miller <chris.miller8823@gmail.com>
2019-12-20 06:25:36 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,8 @@
 # Define the variable board below.
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+
+# Nested Array version
+# Main array with 3 indexes. Each index is an array with 3 indexes
+
+# board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
